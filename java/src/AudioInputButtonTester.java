eddd17721a00396c40855a3bbbe2396d88dec92c
Marco Karier <mkarier@eagles.ewu.edu>
2018-10-26 14:42:24 -0700

    I found this code on Stackoverflow and It seems to accecpt my usb audio input although I didn't get the output to work I feel this is a good starting point for my code since my other code isn't working.

---
+++ java/src/AudioInputButtonTester.java
@@ -0,0 +1,7 @@
+//Copied from Franky Chanyau who posted it on stackoverflow
+public class AudioInputButtonTester {
+  public static void main (String [] args){
+    AudioInputButton myButton = new AudioInputButton();
+    myButton.startListening();
+  }
+}
