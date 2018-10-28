beaf39b98db2203645fe4c3da70e756c68226489
mkarier <mkarier@eagles.ewu.edu>
2018-10-28 15:27:05 -0700

    I removed an old tester so now there is only one file with main.

--- java/src/AudioInputButtonTester.java
+++
@@ -1,7 +0,0 @@
-//Copied from Franky Chanyau who posted it on stackoverflow
-public class AudioInputButtonTester {
-  public static void main (String [] args){
-    AudioInput myButton = new AudioInput();
-    myButton.startListening();
-  }
-}
